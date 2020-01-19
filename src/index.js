1bfdf1b15ad2d699498919070acced5b48830795
Jesus Ochoa <jesus.ochoa@help.com>
2020-01-19 03:49:04 -0600

    Added post first check in

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 'use strict'
 
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const api = express()
+api.use(express.static(__dirname + '/public'))
+api.use('/memes', express.static(__dirname + '/files'))
+api.use(bodyParser.json())
+
 api.listen(3000, () => {
   console.log('Api up and running')
 })
 
-api.get('/', (req, res) => {
-  console.log(req)
-  res.send('Hello, world!')
-})
-
-api.post('/something', (req, res) => {
-  console.log(req)
+api.post('/add', (req, res) => {
+  console.log(req.body)
   res.send('This was a post kajigger')
 })
+
